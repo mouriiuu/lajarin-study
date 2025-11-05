1126d49ef22a18dcfc1f53ee2514d2e8430e9fb6
Nurul <nulaatifa@gmail.com>
2025-11-05 20:02:01 +0800

    Menjawab Soal

---
+++ nulaa.py
@@ -0,0 +1,19 @@
+
+# SOAL 1
+
+umur = 18 # int
+tinggiBadan= 147,8 # float
+mahasiswaAktif= True # boolean
+nama = "Nurul" # string
+Huruf = 'L' # char
+
+#SOAL 2
+
+UMur=int(input("Masukan Umur Anda :  "))
+
+if UMur > 50 :
+    print("Tua Woyy !")
+elif UMur > 17 and UMur < 50 :
+    print("Dewasa Tapi Korupsi!")
+else:
+    print("Aku kecil dan Aku Masih Bahagia!")
